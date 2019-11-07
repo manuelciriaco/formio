28723c17fa20cb626cbd88a972f4a0c90617837e
Manuel Ciriaco GPC <manuelciriaco1@gmail.com>
2019-11-07 10:29:31 -0400

    asd

--- client/dist/admin/application/views/includes/template.php
+++ client/dist/admin/application/views/includes/template.php
@@ -5,7 +5,7 @@
 	);
 ?>
 <?php $this->load->view('includes/sections/header', $data);?>
-<input type="hidden" name="base_url" id="base_url" value="<?php echo base_url('index.php');?>/"/>
+<input type="hidden" name="base_url" id="base_url" value=""/>
 
 <div class="content-wrapper">
 	<div class="div-principal-wrapper wrapper wrapper-content" id="div-principal-wrapper">
